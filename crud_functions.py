dccc074e1c2f2daeac93df60edeb5fc6e8d4bdb5
iralessa <166923860+iralessa@users.noreply.github.com>
2024-09-07 20:20:59 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,27 +1,46 @@
 import sqlite3
-import random
 
+def create_products_table():
+    connection = sqlite3.connect("bot_telegram14_4.db")
+    cursor = connection.cursor()
 
-connection = sqlite3.connect("bot_telegram14_4.db")
-cursor = connection.cursor()
+    # Удаление таблицы Products, если она существует
+    cursor.execute("DROP TABLE IF EXISTS Products")
 
-# Удаление таблицы Users и индекса, если они существуют
-cursor.execute("DROP TABLE IF EXISTS Products")
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Products (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    price INTEGER NOT NULL
-)
-''')
-connection.commit()
-connection.close()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        price INTEGER NOT NULL
+    )
+    ''')
+    connection.commit()
+    connection.close()
+
+def create_users_table():
+    connection = sqlite3.connect("not_telegram3.db")
+    cursor = connection.cursor()
+    
+    # Удаление таблицы Users, если она существует
+    cursor.execute("DROP TABLE IF EXISTS Users")
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        id_user INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER,
+        balance INTEGER NOT NULL
+    )
+    ''')
+    connection.commit()
+    connection.close()
 
 def initiate_db():
-    # """
-    # Заполняет таблицу Products тестовыми данными.
-    # """
+    """
+    Заполняет таблицу Products тестовыми данными.
+    """
     connection = sqlite3.connect("bot_telegram14_4.db")
     cursor = connection.cursor()
     for i in range(1, 5):
@@ -29,6 +48,20 @@ def initiate_db():
         INSERT INTO Products (id, title, description, price)
         VALUES (?, ?, ?, ?)
         ''', (i, f"Продукт {i}", f"Описание {i}", i * 100))
+    connection.commit()
+    connection.close()
+
+def add_user(username, email, age):
+    connection = sqlite3.connect('not_telegram3.db')
+    cursor = connection.cursor()
+
+    # Выполняем SQL-запрос для добавления данных
+    cursor.execute('''
+    INSERT INTO Users (username, email, age, balance)
+    VALUES (?, ?, ?, ?)
+    ''', (username, email, age, 1000))
+
+    # Сохраняем изменения
     connection.commit()
     connection.close()
 
@@ -43,10 +76,29 @@ def get_all_products():
     connection.close()
     return products
 
-# Пример использования
-initiate_db()    # Создание таблицы
-products = get_all_products()
-for k in products:
-    print(k)
+def get_users_count():
+    """
+    Возвращает количество пользователей в таблице Users.
+    """
+    connection = sqlite3.connect("not_telegram3.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT COUNT(*) FROM Users")
+    count = cursor.fetchone()[0]
+    connection.close()
+    return count
 
-# print(get_all_products())  # Получение и вывод всех продуктов
+def is_included(username):
+    """
+    Проверяет, существует ли пользователь с заданным именем в таблице Users.
+
+    :param username: Имя пользователя для проверки.
+    :return: True, если пользователь существует, иначе False.
+    """
+    connection = sqlite3.connect("not_telegram3.db")
+    cursor = connection.cursor()
+    cursor.execute('''
+    SELECT COUNT(*) FROM Users WHERE username = ?
+    ''', (username,))
+    count = cursor.fetchone()[0]
+    connection.close()
+    return count > 0
